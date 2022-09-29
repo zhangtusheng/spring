38918e2e3cc8b5349404b465c5d78f1e123ec864
zhangtusheng <908647593@qq.com>
2022-09-29 09:42:12 +0800

    实现应用上下文，自动识别，资源加载，扩展机制。

--- small-spring-step-06/src/test/java/bean/UserDao.java
+++ small-spring-step-06/src/test/java/bean/UserDao.java
@@ -14,7 +14,7 @@ public class UserDao {
     private static Map<String, String> hashMap = new HashMap<>();
 
     static {
-        hashMap.put("10001", "小傅哥");
+        hashMap.put("10001", "zts");
         hashMap.put("10002", "八杯水");
         hashMap.put("10003", "阿毛");
     }
